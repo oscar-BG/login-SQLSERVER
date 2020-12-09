16835b15fd025a71a4cdb3d73871e1e5062e89ff
Oscar Bautista <oscar04262000@gmail.com>
2020-12-08 22:13:29 -0600

    Cuando nuestro formulario ingresa los datos verifica que el usuario no exita y las contraseñas que da el usuario sean correctas

--- agendaEscritorio/conexionSQL/nuevoUsuario.cs
+++ agendaEscritorio/conexionSQL/nuevoUsuario.cs
@@ -22,29 +22,78 @@ namespace conexionSQL
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
-            //Cadena para conectarnos al servidor
-            SqlConnection cadenaConexion = new SqlConnection("Data Source=LAPTOP-OBEL3V1L; Initial Catalog= usuarioAgenda; Integrated Security= True");
-            //abrimos la conexion
-            cadenaConexion.Open();
-
-            if(cbxTipoCuenta.SelectedIndex == 0)
+            if (txtNombre.Text == "" || txtAPaterno.Text == "" || txtAMaterno.Text == "" || cbxTipoCuenta.SelectedIndex <0 || txtNUsuario.Text == "" || txtPassword.Text == "" || txtCPassword.Text == "")
             {
-                tipoCuenta = "Administrador";
+                MessageBox.Show("Faltan campoas por rellenar");
+               
             }
-            if(cbxTipoCuenta.SelectedIndex == 1)
-            {
-                tipoCuenta = "Invitado";
+            else
+            {//Cadena para conectarnos al servidor
+                SqlConnection cadenaConexion = new SqlConnection("Data Source=LAPTOP-OBEL3V1L; Initial Catalog= usuarioAgenda; Integrated Security= True");
+                
+                String consulaNombreUsuario = "SELECT nombreUsuario FROM db_owner.usuarioSCHARP WHERE nombreUsuario = '"+txtNUsuario.Text+"'";
+                SqlCommand comando2 = new SqlCommand(consulaNombreUsuario,cadenaConexion);
+                cadenaConexion.Open();
+                SqlDataReader lector = comando2.ExecuteReader();
+                if(lector.Read() == true)
+                {
+                    MessageBox.Show("El usuario ya existe Eliga Otro");
+                    cadenaConexion.Close();
+                    txtNUsuario.Clear();
+                    txtPassword.Clear();
+                    txtCPassword.Clear();
+                }
+                else
+                {
+                    //cerramos la conexion
+                    cadenaConexion.Close();
+
+                    if(txtPassword.Text == txtCPassword.Text)
+                    {
+                        if (cbxTipoCuenta.SelectedIndex == 0)
+                        {
+                            tipoCuenta = "Administrador";
+                        }
+                        if (cbxTipoCuenta.SelectedIndex == 1)
+                        {
+                            tipoCuenta = "Invitado";
+                        }
+                        fechaNac = dTPFechaNac.Value.Date.ToString("yyy/MM/dd");
+                        cadenaConexion.Open();
+                        String insertSQL = "insert into db_owner.usuarioSCHARP(nombre, aPaterno, aMaterno, fechaNac, tipoCuenta, nombreUsuario, clave)" +
+                        "VALUES('" + txtNombre.Text + "', '" + txtAPaterno.Text + "', '" + txtAMaterno.Text + "', '" + fechaNac + "', '" + tipoCuenta + "', '" + txtNUsuario.Text + "', '" + txtCPassword.Text + "')";
+
+                        SqlCommand comando = new SqlCommand(insertSQL, cadenaConexion);
+                        comando.ExecuteNonQuery();
+                        cadenaConexion.Close();
+                        Console.WriteLine("Se acompleto la cadena");
+                        cadenaConexion.Close();
+                        limpiar();
+                        MessageBox.Show("Los datos se almacenaron correctamente");
+                    }
+                    else
+                    {
+                        txtPassword.Clear();
+                        txtCPassword.Clear();
+                        MessageBox.Show("Las contraseñas no coinciden, repitelos de nuevo");
+                    }
+                    
+                }
+                
             }
-            fechaNac = dTPFechaNac.Value.Date.ToString("yyy/MM/dd");
+            
+        }
 
-            String insertSQL = "insert into db_owner.usuarioSCHARP(nombre, aPaterno, aMaterno, fechaNac, tipoCuenta, nombreUsuario, clave)" +
-            "VALUES('"+txtNombre.Text +"', '"+txtAPaterno.Text +"', '"+txtAMaterno.Text+"', '"+fechaNac+"', '"+tipoCuenta+"', '"+txtNUsuario+"', '"+txtCPassword.Text+"')";
-
-            SqlCommand comando = new SqlCommand(insertSQL, cadenaConexion);
-            comando.ExecuteNonQuery();
-            cadenaConexion.Close();
-            Console.WriteLine("Se acompleto la cadena");
-            cadenaConexion.Close();
+        //LimpiarComponentes
+        private void limpiar()
+        {
+            txtNombre.Clear();
+            txtAPaterno.Clear();
+            txtAMaterno.Clear();
+            cbxTipoCuenta.SelectedIndex = 0;
+            txtNombre.Clear();
+            txtPassword.Clear();
+            txtCPassword.Clear();
         }
     }
 }
